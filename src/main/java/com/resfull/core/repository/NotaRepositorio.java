0ab45d2d2cc8067c3a1d553e77d74c08fbac44b5
kbaez <kbaez1990@gmail.com>
2018-10-02 23:40:36 -0300

    initial commit

---
+++ src/main/java/com/resfull/core/repository/NotaRepositorio.java
@@ -0,0 +1,15 @@
+package com.resfull.core.repository;
+
+import com.resfull.core.entity.Nota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Repository("repositorio")
+public interface NotaRepositorio extends JpaRepository<Nota, Serializable> {
+    public abstract Nota findByNombre(String nombre);
+    public abstract List<Nota> findByTitulo(String titulo);
+    public abstract Nota findByNombreAndTitulo(String nombre, String titulo);
+}
---
+++ src/main/java/com/resfull/core/service/NotaService.java
@@ -0,0 +1,55 @@
+package com.resfull.core.service;
+
+import com.resfull.core.converter.Convertidor;
+import com.resfull.core.entity.Nota;
+import com.resfull.core.model.MNota;
+import com.resfull.core.repository.NotaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("servicio")
+public class NotaService {
+    @Autowired
+    @Qualifier("repositorio")
+    private NotaRepositorio repositorio;
+
+    @Autowired
+    @Qualifier("convertidor")
+    private Convertidor convertidor;
+
+    public boolean crear(Nota nota){
+        try{
+            repositorio.save(nota);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public boolean actualizar(Nota nota){
+        try{
+            if(nota.getId() == 0){
+                repositorio.save(nota);
+            }
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public boolean borrar(String nombre, long id){
+        try{
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public List<MNota> obtener(){
+        List<MNota> notas = null;
+        return notas;
+    }
+}
