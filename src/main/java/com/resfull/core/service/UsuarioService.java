319e8f22358b21f18c59793c41f594a2603f0046
kbaez <kbaez1990@gmail.com>
2018-10-08 23:45:21 -0300

    add spring security

--- src/main/java/com/resfull/core/service/UsuarioService.java
+++ src/main/java/com/resfull/core/service/UsuarioService.java
@@ -1,14 +1,23 @@
 package com.resfull.core.service;
 
+import com.resfull.core.entity.Usuario;
 import com.resfull.core.repository.GestorUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service("usuarioservice")
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("usuarioService")
 public class UsuarioService implements UserDetailsService {
 
     @Autowired
@@ -17,7 +26,20 @@ public class UsuarioService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        repo.findByUsuario(userName);
-        return null;
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        Usuario user = repo.findByUsuario(userName);
+        return new User(user.getUsuario(),  encoder.encode(user.getContrasena()),
+                user.isActivo(), user.isActivo(), user.isActivo(), user.isActivo(),buildgrante(user.getRol()));
+    }
+
+    public List<GrantedAuthority> buildgrante(byte rol){
+        String[] roles = {"LECTOR", "USUARIO","ADMINISTRADOR"};
+        List<GrantedAuthority> auths = new ArrayList<>();
+
+        for(int i = 0; i < rol; i++){
+            auths.add(new SimpleGrantedAuthority(roles[i]));
+        }
+        return auths;
     }
 }
+
